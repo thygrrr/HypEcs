c8437342d62dd3fe0a8001a34ca16b35eb4f273b
Tiger Jove <tiger@jupiter.blue>
2024-02-21 00:52:56 +0100

    Removed old RefAction remnants

--- fennecs/Query.cs
+++ fennecs/Query.cs
@@ -157,32 +157,3 @@ public delegate void SpanAction_CCU<C0, C1, U>(Span<C0> c0, Span<C1> c1, in U un
 public delegate void SpanAction_CCCU<C0, C1, C2, U>(Span<C0> c0, Span<C1> c1, Span<C2> c2, in U uniform);
 public delegate void SpanAction_CCCCU<C0, C1, C2, C3, U>(Span<C0> c0, Span<C1> c1, Span<C2> c2, Span<C3> c3, in U uniform); 
 public delegate void SpanAction_CCCCCU<C0, C1, C2, C3, C4, U>(Span<C0> c0, Span<C1> c1, Span<C2> c2, Span<C3> c3, Span<C4> c4, in U uniform);
-
-
-// ReSharper enable IdentifierTypo
-// ReSharper enable InconsistentNaming
-
-// ReSharper disable CommentTypo
-/* TODO: These would be used for "early out" and search type algorithms.
- public delegate void RefAction_CS<C0>(ref C0 comp0, ParallelLoopState state);
-
-public delegate void RefAction_CCS<C0, C1>(ref C0 comp0, ref C1 comp1, ParallelLoopState state);
-
-public delegate void RefAction_CCCS<C0, C1, C2>(ref C0 comp0, ref C1 comp1, ref C2 comp2, ParallelLoopState state);
-
-public delegate void RefAction_CCCCS<C0, C1, C2, C3>(ref C0 c0, ref C1 c1, ref C2 c2, ref C3 c3, ParallelLoopState state);
-
-public delegate void RefAction_CCCCCS<C0, C1, C2, C3, C4>(ref C0 c0, ref C1 c1, ref C2 c2, ref C3 c3, ref C4 c4, ParallelLoopState state);
-
-
-public delegate void RefAction_CUS<C0, in U>(ref C0 comp0, U uniform, ParallelLoopState state);
-
-public delegate void RefAction_CCUS<C0, C1, in U>(ref C0 comp0, ref C1 comp1, U uniform, ParallelLoopState state);
-
-public delegate void RefAction_CCCUS<C0, C1, C2, in U>(ref C0 comp0, ref C1 comp1, ref C2 comp2, U uniform, ParallelLoopState state);
-
-public delegate void RefAction_CCCCUS<C0, C1, C2, C3, in U>(ref C0 c0, ref C1 c1, ref C2 c2, ref C3 c3, U uniform, ParallelLoopState state);
-
-public delegate void RefAction_CCCCCUS<C0, C1, C2, C3, C4, in U>(ref C0 c0, ref C1 c1, ref C2 c2, ref C3 c3, ref C4 c4, U uniform, ParallelLoopState state);
-*/
-// ReSharper enable CommentTypo
