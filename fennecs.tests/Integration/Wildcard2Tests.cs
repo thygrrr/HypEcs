6f26a9200c53e97c3f1668a3c094227657f75b87
Tiger Jove <tiger@jupiter.blue>
2024-02-23 18:17:05 +0100

    Made tests more scalable, but need to revisit for Q2

---
+++ fennecs.tests/Integration/Wildcard2Tests.cs
@@ -0,0 +1,114 @@
+﻿namespace fennecs.tests.Integration;
+
+public class Wildcard2Tests
+{
+    private readonly World _world;
+
+    private readonly Entity _entity;
+    
+    // string may be interned or not
+    const string OBJECT1 = "hello world";
+    const string OBJECT2 = "fly, you fools";
+    const string NONE1 = "can't touch this";
+    const string RELATION1 = "IOU";
+    
+    public Wildcard2Tests()
+    {
+        _world = new World();
+        
+        var bob = _world.Spawn().Id();
+        _entity = _world.Spawn().Add<float>().AddLink(OBJECT1).AddLink(OBJECT2).Add(NONE1).AddRelation(bob, RELATION1).Id();
+    }
+    
+    [Fact]
+    public void Wildcard_Any_Enumerates_all_Components_Once()
+    {
+        using var query = _world.Query<string, float>(Entity.Any).Build();
+        
+        HashSet<string> seen = [];
+        query.ForEach((ref string str, ref float _) =>
+        {
+            Assert.DoesNotContain(str, seen);
+            seen.Add(str);
+        });
+        Assert.Contains(OBJECT1, seen);
+        Assert.Contains(OBJECT2, seen);
+        Assert.Contains(NONE1, seen);
+        Assert.Contains(RELATION1, seen);
+        Assert.Equal(4, seen.Count);
+    }
+    
+
+    [Fact]
+    public void Wildcard_None_Enumerates_Only_Plain_Components()
+    {
+        using var query = _world.Query<string, float>(Entity.None).Build();
+
+        HashSet<string> seen = [];
+        query.ForEach((ref string str, ref float _) =>
+        {
+            Assert.DoesNotContain(str, seen);
+            seen.Add(str);
+        });
+        Assert.Contains(NONE1, seen);
+        Assert.Single(seen);
+    }
+
+
+    [Fact]
+    public void Wildcard_Target_Enumerates_all_Relations()
+    {
+        using var query = _world.Query<string, float>(Entity.Target).Build();
+
+        HashSet<string> seen = [];
+
+        query.ForEach((ref string str, ref float _) =>
+        {
+            Assert.DoesNotContain(str, seen);
+            seen.Add(str);
+        });
+        Assert.Contains(OBJECT1, seen);
+        Assert.Contains(OBJECT2, seen);
+        Assert.Contains(RELATION1, seen);
+        Assert.Equal(3, seen.Count);
+    }
+
+
+
+    [Fact]
+    public void Wildcard_Relation_Enumerates_all_Relations()
+    {
+        using var query = _world.Query<string, float>(Entity.Relation).Build();
+        
+        HashSet<string> seen = [];
+
+        query.ForEach((ref string str, ref float _) =>
+        {
+            Assert.DoesNotContain(str, seen);
+            seen.Add(str);
+        });
+        Assert.Contains(RELATION1, seen);
+        Assert.Single(seen);
+    }
+
+
+
+    [Fact]
+    public void Wildcard_Object_Enumerates_all_Object_Links()
+    {
+        using var query = _world.Query<string, float>(Entity.Object).Build();
+
+        HashSet<string> seen = [];
+
+        query.ForEach((ref string str, ref float _) =>
+        {
+            Assert.DoesNotContain(str, seen);
+            seen.Add(str);
+            Assert.True(ReferenceEquals(OBJECT1, str) || ReferenceEquals(OBJECT2, str));
+        });
+        
+        Assert.Contains(OBJECT1, seen);
+        Assert.Contains(OBJECT2, seen);
+        Assert.Equal(2, seen.Count);
+    }
+}
